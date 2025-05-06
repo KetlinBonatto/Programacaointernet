b35c1957164cd095ad72bd9cf78a3e461b77d3cd
KetlinBonatto <ketlin160307@edu.unipar.br>
2025-05-05 21:37:00 -0300

    Add files via upload

---
+++ java/script.js
@@ -0,0 +1,18 @@
+let h1Text = document.querySelector("#h1Text");
+let inputText = document.querySelector("#inputText");
+let btTrocaText = document.querySelector("#btTrocaText");
+
+function trocarText(){
+    //retornando o texto digitado no campo
+    let textoDigitado = inputText.value;
+
+    //Convertendo o valor retornado no input em número
+    let num = Nuber(imputTexto.value);
+
+    //Alterando o texto do elemento h1
+    h1Text.textContent = textoDigitado;
+}
+
+btTrocaText.onclick = function(){
+    trocarText(); 
+}
