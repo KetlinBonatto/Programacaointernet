f9d9c1f30c3b53a8829d21382db0c1f4c0aaed4d
KetlinBonatto <ketlin160307@edu.unipar.br>
2025-05-05 22:03:40 -0300

    Add files via upload

---
+++ ex1java/script.js
@@ -0,0 +1,16 @@
+let inputSoma = document.querySelector("#inputSoma")
+
+let inputNumero = document.querySelector("#inputNumero")
+
+let btMais = document.querySelector("#btMais")
+
+let inputResultado = document.querySelector("#inputResultado")
+
+function soma(){
+    //retornando o texto digitado no campo
+    let textoDigitado = inputText.value;
+
+    //Alterando o texto do elemento h1
+    let num = Nuber(imputTexto.value);
+
+}
